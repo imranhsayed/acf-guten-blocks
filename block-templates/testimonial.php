3d25de752457cce2a87896545713b9962fca88a9
Imran Sayed <imranhsayed@gmail.com>
2019-10-08 22:05:13 +0530

    Update the template file

--- block-templates/testimonial.php
+++ block-templates/testimonial.php
@@ -12,37 +12,22 @@ if ( empty( $block['id'] ) ) {
 }
 
 // Create id attribute allowing for custom 'anchor' value.
-$id = ( ! empty( $block['anchor'] ) ) ?
-		$block['anchor'] :
-		sprintf( 'testimonial-%s', $block['id'] );
+$id = ( ! empty( $block['anchor'] ) ) ? $block['anchor'] : sprintf( 'testimonial-%s', $block['id'] );
 
 // Create class attribute allowing for custom "className" and "align" values.
-$className = ( ! empty( $block['className'] ) ) ?
-			    sprintf( 'testimonial %s', $block['className'] ) :
-			    'testimonial';
-
-$className = ( ! empty( $block['align'] ) ) ?
-			   sprintf( '%1$s align%2$s', $className, $block['align'] ) :
-	           $className;
-
+$className = ( ! empty( $block['className'] ) ) ? sprintf( 'testimonial %s', $block['className'] ) : 'testimonial';
+$className = ( ! empty( $block['align'] ) ) ? sprintf( '%1$s align%2$s', $className, $block['align'] ) : $className;
 
 // Load values and assigning defaults.
 $text = get_field( 'testimonial' ) ?: 'Your testimonial here...';
-
 $author = get_field( 'author' ) ?: 'Author name';
-
 $role = get_field( 'role' ) ?: 'Author role';
-
 $image = get_field( 'image' ) ?: '';
-
 $background_color = get_field( 'background_color' );
-
 $text_color = get_field( 'text_color' );
-
 ?>
 
 	<div id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $className ); ?>">
-
 		<blockquote class="testimonial-blockquote">
 			<span class="testimonial-text"><?php echo esc_html( $text ); ?></span>
 			<span class="testimonial-author"><?php echo esc_html( $author ); ?></span>
